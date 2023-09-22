64f0df113b72b7e898ea71b8baf92ea9064a323e
pedro <paynemax@163.com>
2023-09-22 18:53:27 +0800

    add README.md also minor update in Home.js

--- src/Home.js
+++ src/Home.js
@@ -3,38 +3,7 @@ import DeviceList from "./DeviceList";
 import { useEffect } from "react";
 
 const Home = () => {
-  const [devices, setDevices] = useState([
-    {
-      id: 1,
-      name: "Ezone001",
-      distance: 20,
-      threshold: 22,
-      empty_distance: 23,
-      desc: "located on F3 Ezone",
-      created_at: "18-09-2023 10:10:10",
-      updated_at: "18-09-2023 10:10:10",
-    },
-    {
-      id: 2,
-      name: "Ezone002",
-      distance: 22.5,
-      threshold: 22,
-      empty_distance: 23,
-      desc: "Be careful about this one!",
-      created_at: "18-09-2023 10:10:10",
-      updated_at: "18-09-2023 10:10:10",
-    },
-    {
-      id: 3,
-      name: "Reid001",
-      distance: 12,
-      threshold: 22,
-      empty_distance: 23,
-      desc: "",
-      created_at: "18-09-2023 10:10:10",
-      updated_at: "18-09-2023 10:10:10",
-    },
-  ]);
+  const [devices, setDevices] = useState([]);
 
   useEffect(() => {
     fetch("https://virtserver.swaggerhub.com/ArsenePadthai/iot/1.0.0/devices")
@@ -51,7 +20,7 @@ const Home = () => {
 
   return (
     <div className="home container">
-      <DeviceList devices={devices}/>
+      {devices && <DeviceList devices={devices}/>}
     </div>
   );
 };
